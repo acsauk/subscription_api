438df69ff177d27cf3259380a32365d5488848f4
Alex Saunders <acsauk@gmail.com>
2018-03-08 20:28:24 +0000

    creates failing test for subscribing a phone to a product_id, adds test script to composer.json

---
+++ tests/Feature/SubscribeAPhoneTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubscribeAPhoneTest extends TestCase
+{
+    /** @test */
+    public function user_can_subscribe_a_phone_to_product_id()
+    {
+        // Arrange
+        $msisdn = '07535123123';
+        $product_id = 'productid1';
+
+        // Act
+        $response = $this->post("/api/v1/subscribe?msisdn=$msisdn&product_id=$product_id'");
+
+        // Assert
+        $response->assertStatus(200)
+          ->assertJson(['msisdn' => $msisdn, 'product_id' => $product_id, 'active' => true]);
+    }
+}
