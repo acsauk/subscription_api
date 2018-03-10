c5dc7c2ab6a8ba3824dff0ec879e619ae034543e
Alex Saunders <acsauk@gmail.com>
2018-03-10 21:01:38 +0000

    creates failing test for search on msisdn and product_id

--- tests/Feature/SubscriptionTest.php
+++ tests/Feature/SubscriptionTest.php
@@ -46,4 +46,20 @@ class SubscriptionTest extends TestCase
         $updated_subscription = Subscription::find($active_subscription);
         $this->assertEquals($active_subscription->active, 0);
     }
+
+    /** @test */
+    public function user_can_search_for_subscription_with_msisdn_and_product_id()
+    {
+        // Arrange
+        $active_subscription = Subscription::all()->first();
+        $msisdn = $active_subscription->msisdn;
+        $product_id = $active_subscription->product_id;
+
+        // Act
+        $response = $this->get("/api/subscriptions?msisdn={$msisdn}&product_id={$product_id}");
+
+        // Assert
+        $response->assertStatus(200)
+          ->assertJson(['msisdn' => $msisdn, 'product_id' => $product_id, 'active' => 1]);
+    }
 }
