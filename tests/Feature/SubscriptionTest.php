d47265c5cd7d7c673fa6f263bea5f33ca7e25a13
Alex Saunders <acsauk@gmail.com>
2018-03-10 23:15:22 +0000

    adds test for international phone number subscription

--- tests/Feature/SubscriptionTest.php
+++ tests/Feature/SubscriptionTest.php
@@ -31,6 +31,26 @@ class SubscriptionTest extends TestCase
     }
 
     /** @test */
+    public function user_can_subscribe_a_phone_to_product_id_international_format()
+    {
+        // Arrange
+        $msisdn = '+447535123123';
+        $product_id = 'productid1';
+
+        // Act
+        $response = $this->get("/api/subscriptions/subscribe?msisdn={$msisdn}&product_id={$product_id}");
+
+        // Assert
+        $response->assertStatus(201)
+          ->assertJson(['msisdn' => $msisdn, 'product_id' => $product_id]);
+
+        $content = json_decode($response->getContent(), true);
+
+        $subscription = Subscription::find($content['id']);
+        $this->assertEquals($subscription->active, 1);
+    }
+
+    /** @test */
     public function user_can_unsubscribe_a_phone_from_product_id()
     {
         // Arrange
