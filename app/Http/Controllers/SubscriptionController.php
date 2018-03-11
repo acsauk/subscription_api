65a04f9e478bc6b366c8c5476c92dfa8c6ab4f88
Alex Saunders <acsauk@gmail.com>
2018-03-11 23:10:37 +0000

    creates migration to add subscribed_date to subscriptions, adds as fillable filed in model and passes test

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -5,68 +5,73 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Subscription;
 use GuzzleHttp\Client;
+use Carbon\Carbon;
 
 class SubscriptionController extends Controller
 {
     public function subscribe(Request $request)
     {
-      return Subscription::create($request->all());
+        return Subscription::create([
+          'msisdn' => $request->input('msisdn'),
+          'product_id' => $request->input('product_id'),
+          'subscribed_date' => Carbon::now()->format('Y-m-d')
+        ]);
     }
 
     public function unsubscribe(Request $request)
     {
-      $msisdn = $request->input('msisdn');
-      $product_id = $request->input('product_id');
+        $msisdn = $request->input('msisdn');
+        $product_id = $request->input('product_id');
 
-      $subscription = $this->get_subscription($msisdn, $product_id);
-      $subscription->active = 0;
-      $subscription->save();
+        $subscription = $this->get_subscription($msisdn, $product_id);
+        $subscription->active = 0;
+        $subscription->save();
 
-      return $subscription;
+        return $subscription;
     }
 
     public function search(Request $request)
     {
-      $msisdn = $request->input('msisdn') ?: '';
-      $product_id = $request->input('product_id') ?: '';
+        $msisdn = $request->input('msisdn') ?: '';
+        $product_id = $request->input('product_id') ?: '';
 
-      if($msisdn && $product_id) {
-        return $this->get_subscription($msisdn, $product_id);
-      } elseif ($msisdn && $product_id == '') {
-        return $this->get_subscriptions_by_msisdn($msisdn);
-      } elseif ($msisdn == '' && $product_id) {
-        return $this->get_subscriptions_by_product_id($product_id);
-      }
+        if($msisdn && $product_id) {
+            return $this->get_subscription($msisdn, $product_id);
+        } elseif ($msisdn && $product_id == '') {
+            return $this->get_subscriptions_by_msisdn($msisdn);
+        } elseif ($msisdn == '' && $product_id) {
+            return $this->get_subscriptions_by_product_id($product_id);
+        }
     }
 
     private function get_subscriptions_by_msisdn($msisdn)
     {
-      $alias = $msisdn;
+        $alias = $msisdn;
 
-      if($msisdn[0] != 'A') {
-        $alias = $this->get_msisdn_alias($msisdn);
-      }
+        if($msisdn[0] != 'A') {
+            $alias = $this->get_msisdn_alias($msisdn);
+        }
 
-      return Subscription::where('msisdn', $msisdn)
-                         ->orWhere('msisdn', $alias)
-                         ->get();
+        return Subscription::where('msisdn', $msisdn)
+                           ->orWhere('msisdn', $alias)
+                           ->get();
     }
 
     private function get_subscriptions_by_product_id($product_id)
     {
-      return Subscription::where('product_id', $product_id)->get();
+        return Subscription::where('product_id', $product_id)->get();
     }
 
     private function get_subscription($msisdn, $product_id)
     {
-      return Subscription::where('msisdn', $msisdn)
+        return Subscription::where('msisdn', $msisdn)
                          ->where('product_id', $product_id)
                          ->first();
     }
 
     private function get_msisdn_alias($msisdn)
     {
-      $client = new Client();
-      return $client->request('GET', "http://interview.pmcservices.co.uk/alias/lookup?msisdn={$msisdn}")->getBody();
+        $client = new Client();
+        return $client->request('GET', "http://interview.pmcservices.co.uk/alias/lookup?msisdn={$msisdn}")->getBody();
     }
 }
--- app/Subscription.php
+++ app/Subscription.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    protected $fillable = ['msisdn', 'product_id', 'active'];
+    protected $fillable = ['msisdn', 'product_id', 'active', 'subscribed_date'];
 }
