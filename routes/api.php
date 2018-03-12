a3278e83c940d41043d2f5e44ca2cca5fa990b31
Alex Saunders <acsauk@gmail.com>
2018-03-12 20:35:57 +0000

    removes some boilerplate from routes

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,6 @@ use Illuminate\Http\Request;
 use App\Subscription;
 use App\Htpp\Controllers\SubscriptionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/subscriptions/subscribe', 'SubscriptionController@subscribe');
 Route::post('/subscriptions/subscribe', 'SubscriptionController@subscribe');
 
